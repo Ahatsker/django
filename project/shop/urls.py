916fbb9b7f1165bcd8ddf280028784c86ef761fe
Akhat <kulzanovahat@gmail.com>
2023-09-08 20:48:37 +0600

    lesson2

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 from project import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('help', views.help, name='help'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
