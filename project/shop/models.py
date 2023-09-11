309e8ca4efc5374d6327e863e1778447e540e70c
Akhat <kulzanovahat@gmail.com>
2023-09-11 20:49:55 +0600

    lesson3

--- project/shop/models.py
+++ project/shop/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Shop(models.Model):
     name = models.CharField(verbose_name="Название товара", max_length=60)
     price = models.IntegerField(verbose_name="Цена")
-    photo_path = models.ImageField(verbose_name="Фото товара", upload_to="photo/%Y/%m/%d/")
+    photo_path = models.ImageField(verbose_name="Фото товара", upload_to="photo/%Y/%m/%d/", default='media/photo/2023/09/08/pgadmin.png')
 
     def __str__(self):
         return self.name
