aa3d54f9154434542a2e3e5eb561c2e63fb7c328
Akhat <kulzanovahat@gmail.com>
2023-09-20 19:33:19 +0600

    l5

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,5 +20,5 @@ from shop.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
+    path('', include('shop.urls')),
 ]
--- project/shop/models.py
+++ project/shop/models.py
@@ -7,6 +7,23 @@ class Shop(models.Model):
     name = models.CharField(verbose_name="Название товара", max_length=60)
     price = models.IntegerField(verbose_name="Цена")
     photo_path = models.ImageField(verbose_name="Фото товара", upload_to="photo/%Y/%m/%d/", default='/photo/2023/09/08/pgadmin.png')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория", null=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name ="Товар"
+        verbose_name_plural = "Товары"
+        ordering = ['name']
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="Категория", max_length=50, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name ="Категория"
+        verbose_name_plural = "Категории"
